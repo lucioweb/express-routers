8d74e0dc53f3dcfcf17a9f5e8ef9670cc96023bc
luciolemos <lucio.lemos.385@ufrn.edu.br>
2023-09-14 20:38:48 -0300

    commit

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ app.get('/alunos', (req, res) => {
     res.json({ alunos: Object.values(alunos) })
 })
 
-app.listen(8080, () => {
-    console.log('Servidor pronto na porta 8080')
+app.listen(3000, () => {
+    console.log('Servidor pronto na porta 3000')
 })
